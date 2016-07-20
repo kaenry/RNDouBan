88de8241c0beb50ed8b28da8b46d42cab64a9040
kaenry <kaenry@163.com>
2016-07-20 18:36:01 +0800

    add user index

--- app/user/index.js
+++ app/user/index.js
@@ -1,24 +1,51 @@
 import React, {
-  PropTypes,
+    PropTypes,
 } from 'react';
 
 import {
-  View,
-  Text
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    Stylesheet,
 } from 'react-native';
 
 export default class User extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>
-          User
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <View style={[styles.avatarView]}>
+                  <TouchableOpacity style={styles.avatarTouch}>
+                      <Image source={{uri: 'http://userimg.yidijie.com/animal-avatar11.jpg'}} style={styles.avatar}></Image>
+                      <Text style={styles.avatarText}>点击修改头像</Text>
+                  </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
 User.propTypes = {};
 
 User.defaultProps = {};
+
+const styles = StyleSheet.create({
+    avatarTouch: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    avatarText: {
+        fontSize: 14,
+        marginLeft: 20,
+    },
+    avatarView: {
+        marginTop: 1,
+        paddingTop: 20,
+        paddingBottom: 20,
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+    }
+});
